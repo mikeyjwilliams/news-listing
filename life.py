bbd5f7a7ba0df3d8e89608878f80aad2ccd57929
Mikey Williams <williamsmikeyj@outlook.com>
2024-02-19 13:36:45 -0500

    add

---
+++ life.py
@@ -0,0 +1,18 @@
+
+
+
+class Human:
+    def __init__(self):
+        self.name: str = ("name: ")
+        self.age: int = ("age: ")
+        self.gender: str = ("gender (M/F): ")
+        self.height: int = ("height in inches "))
+        self.weight: float = ("weight: nearest pound"))
+        self.eyes = ("eyes: ")
+        self.teeth = ("teeth: ")
+        self.hair = ("hair: ")
+        self.eyes_color = ("eyes color: ")
+        self.teeth_color = ("teeth color: ")
+        self.hair_color = ("hair color: ")
+        self.skin_color = ("skin color: ")
+        self.hair_length = int ("hair length: "))
