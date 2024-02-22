3f9ab2bde47643cc276275ce407042b010781532
Mikey Williams <williamsmikeyj@outlook.com>
2024-02-22 18:28:15 -0500

    running module human in main for tests and features

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-
+from human import Human
 
 def main():
-    pass
-
-
-
+    j = Human(name='John Doe', age=24, gender='M', height_in_inches=75)
+    j.weight_setter(125)
+    print( j.weight_getter(), j.name_getter())
+    
+   
+    
 
 if __name__ == '__main__':
     main()
