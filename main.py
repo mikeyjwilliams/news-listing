4112006ce551516e919510779138c8a5b7aa184c
Mikey Williams <williamsmikeyj@outlook.com>
2024-02-22 18:32:14 -0500

    comment out exploratory  testing in main file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from human import Human
 
 def main():
-    j = Human(name='John Doe', age=24, gender='M', height_in_inches=75)
-    j.weight_setter(125)
-    print( j.weight_getter(), j.name_getter())
+    pass 
+    # j = Human(name='John Doe', age=24, gender='M', height_in_inches=75)
+    # j.weight_setter(125)
+    # print( j.weight_getter(), j.name_getter())
     
    
     
