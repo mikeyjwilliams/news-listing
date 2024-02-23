74c004ad561fc968b71b826ac5f92256ed01f886
Mikey Williams <williamsmikeyj@outlook.com>
2024-02-22 19:30:48 -0500

    add feature get_first & get_last name fix if no last name

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from utils.human import Human
 
 def main():
-     
-    john = Human(name='John Doe', age=24, gender='M', height_in_inches=75)
-    john.weight_setter(125)
-    print(john.get_first_name())
-    print('----')
-    print(john.get_last_name())
+     pass
     
     
     
